f75afbb3df966dcfd0f5acd27df321040189815c
rangaimal <rangaimal1234@gmail.com>
2018-04-30 11:15:10 +0530

    add all again

--- login page/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$connection=mysqli_connect('localhost','root','','demo');
-if(mysqli_connect_errno()){
-	die('database connection failed'.mysqli_connect_error());
-}else{
-	//echo"connection succesful";
-}
-?> 
